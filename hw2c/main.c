a4d7aa7c73c9cccb5588474dd23d96546b0dc2bb
Arsen <arsen.kitov@gmail.com>
2018-09-30 15:52:55 +0300

    minor fixes + comments translated

--- hw2c/main.c
+++ hw2c/main.c
@@ -1,3 +1,15 @@
+/*
+  Китов Арсен, АПО-11, ДЗ N1
+  Задание B. Вариант 3.
+  Требуется написать программу, которая способна вычислять арифметические выражения.
+  Выражения могут содержать:
+  1) знаки операций '+', '-', '/', '*'
+  2) Скобки '(', ')'
+  3) Целые и вещественные числа, в нотации '123', '123.345', все операции должны быть вещественны, а результаты выведены с точностю до двух знаков после запятой в том числе целые '2.00'
+  4) необходимо учитывать приоритеты операций, и возможность унарного минуса, пробелы ничего не значат
+  5) Если в выражении встретилась ошибка требуется вывести в стандартный поток вывода "[error]" (без кавычек)
+*/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -17,11 +29,12 @@ typedef enum {
   TT_BRACE = 3
 } token_kind_t;
 
+// если я правильно понимаю, это тратит sizeof(double) * 2 места. В целом так можно делать, или стоит делать как-то иначе
 typedef struct {
   double number;
   char value;
   token_kind_t kind;
-} token_t; //wasting lots of space (3 bytes out of 16 (?)) but idk how to do it better
+} token_t;
 
 typedef struct {
   token_t * head;
@@ -43,14 +56,14 @@ double compute_operator(char operation, double operand1, double operand2)
     case '*':
       return operand1 * operand2;
     case '/':
-      assert(operand2 != 0); // should think of some other way to report this
+      assert(operand2 != 0); // наверное стоит проверять каким-то другим способом, но пусть пока так
       return operand1 / operand2;
     default:
       return 0;
   }
 }
 
-double compute_function(char func, double arg) //just in case i'll add more
+double compute_function(char func, double arg)
 {
   assert(func == 'i');
   switch(func) {
@@ -85,7 +98,7 @@ int push(token_stack_t * stack, token_t value) {
   return 0;
 }
 
-int pop (token_stack_t * stack) { // does not return the popped element, just the 0/-1/-2
+int pop (token_stack_t * stack) { // не возвращает удаленный элемент, просто 0/-1/-2
   assert(stack != NULL && (stack->head != NULL || (stack->real_size == 0 && stack->max_size == 0)));
   if (stack->real_size == 0) {
     return -2;
@@ -105,7 +118,7 @@ int pop (token_stack_t * stack) { // does not return the popped element, just th
   return 0;
 }
 
-bool is_zero(const char * ch) // doesn't check for double dot, since it's checked in the algorithm before calling atof
+bool is_zero(const char * ch) // нужно для проверки валидности atof, хотя если алгоритм правилен, вызов всегда валиден
 {
   assert(ch != NULL);
   for (size_t i = 0; ch[i] != '\0'; ++i)
@@ -113,7 +126,7 @@ bool is_zero(const char * ch) // doesn't check for double dot, since it's checke
     if (ch[i] != '0' && ch[i] != '.')
       return false;
   }
-  return ch[0] == '0'; //check for empty string and start with a dot
+  return ch[0] == '0'; //проверка на пустую строчку и точку
 }
 
 bool is_operation(char c)
@@ -130,7 +143,7 @@ bool is_digit(char c)
 /**ACTUAL ALGORITHM: read one token, read a string, convert string to reverse polish notation, compute polish**/
 /**************************************************************************************************************/
 
-int read_expr(char ** line, bool ignore_newline) { //reads line, ignores spaces
+int read_expr(char ** line, bool ignore_newline) { // читает без пробелов до EOF или конца строки
   assert(line != NULL);
   if (*line != NULL)
     free(line);
@@ -148,13 +161,13 @@ int read_expr(char ** line, bool ignore_newline) { //reads line, ignores spaces
       if (*line == NULL)
         return -1;
       strcpy(*line, buffer);
-    } else if (chars_read == BUFFER_SIZE - 2) // if line was allocated, the loop ends, so no need to free
+    } else if (chars_read == BUFFER_SIZE - 2) // если был malloc, то из цикла уже вышли
       return -1;
-    else if (!isspace(x)) // order is important, check for \n first
+    else if (!isspace(x)) // идет после проверки на \n - порядок важен
     {
       buffer[chars_read] = (char)x;
       ++chars_read;
-    } // else (x is a whitespace) do nothing
+    } // иначе (x = пробел/таб) ничего не делаем
   }  while( x != EOF && (x != '\n' || ignore_newline));
   if (ferror(stdin))
   {
@@ -164,11 +177,10 @@ int read_expr(char ** line, bool ignore_newline) { //reads line, ignores spaces
   return chars_read;
 }
 
-// -2 for invalid string, -1 for other errors
-// polish expr isn't a stack - it's actually a queue. But we only push, and reading will be done
-// in reverse order, so it's just a little more convenient than a simple token_t **
-// and I don't have to write push/pop for queue :P
-// does NOT free polish_expr in case of an error (has to be done manually)
+// -2 - неверная строка, -1 - ошибка памяти или чего-то еще
+// polish_expr это на самом деле не стэк, а просто динамический массив - из него никогда не удаляются значения
+// но использовать тот же struct проще
+// функция не освобождает ни polish_expr, ни expr в случае ошибки
 int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
 {
   assert(expr != NULL && polish_expr != NULL && polish_expr->head == NULL);
@@ -191,7 +203,8 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
         dot_passed = false;
       }
     } else if (expr[i] == '.') {
-      // is i == 0 || is_digit(expr[i-1]) safe or should it be nested
+      // не уверен, можно ли так делать (i==0 || expr[i-1]), но в целом работает
+      // помню, в c++ точно порядок задан, по сям не смог нормальный ответ нагуглить
       if (dot_passed || !reading_number || i == 0 || !is_digit(expr[i-1])) {
         free(tokens.head);
         return -2;
@@ -201,7 +214,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
     } else {
       if (reading_number) {
         reading_number = false;
-        if (i - first_digit >= BUFFER_SIZE) {  // can't fit, 10^31 will overflow anyways
+        if (i - first_digit >= BUFFER_SIZE) {  // 30 знаков в double все равно не влезет
           free(tokens.head);
           return -1;
         }
@@ -209,7 +222,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
         buffer[i - first_digit] = '\0';
         tmp_token.kind = TT_NUMBER;
         tmp_token.number = atof(buffer);
-        assert(tmp_token.number != 0 || is_zero(buffer)); // if the algorithm is correct, this is always true
+        assert(tmp_token.number != 0 || is_zero(buffer)); // всегда верно, если алгоритм работает правильно
         error_st = push(polish_expr, tmp_token);
         if (error_st < 0) {
           free(tokens.head);
@@ -218,7 +231,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
       }
       //same i==0 || expr[i-1] as above
       if (expr[i] == '-' && (i == 0 || !(is_digit(expr[i-1]) || expr[i-1] == ')'))) {
-        if (i != 0 && expr[i-1] != '(') { // this invalidates 3 + -2 in favor of 3 + (-2)
+        if (i != 0 && expr[i-1] != '(') { // из-за этой строчки конструкции вида 3+-2 неверны (надо 3 + (-2))
           free(tokens.head);
           return -1;
         }
@@ -260,7 +273,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
           return error_st;
         }
       } else if (expr[i] == ')') {
-        if (tokens.real_size == 0) { // only need to check once, inside the loop it's checked in pop()
+        if (tokens.real_size == 0) { // проверяем на пустоту всего разок, внутри проверяется в pop()
           free(tokens.head);
           return -2;
         }
@@ -277,7 +290,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
               return error_st;
             }
         }
-        // actually tokens can't be empty, but the realloc inside of it can return -1, so have to check
+        // tokens, не может быть пустым, но все равно проверяем, если realloc в pop вернет nullptr
         error_st = pop(&tokens);
         if (error_st < 0) {
           free(tokens.head);
@@ -296,15 +309,15 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
             return error_st;
           }
         }
-      } else { // expr[i] is none of the 0-9+-*/() - throw
+      } else { // expr[i] не входит в 0-9+-*/() - убираем за собой и выбрасываем ошибку
         free(tokens.head);
         return -2;
       }
     }
 
   }
-  if (reading_number) { // if the expression ends with a number, we haven't read it
-    if (i - first_digit >= BUFFER_SIZE) {  // can't fit, 10^31 will overflow anyways
+  if (reading_number) { // если выражение заканчивается на число, мы его еще не прочитали
+    if (i - first_digit >= BUFFER_SIZE) {  // снова выбрасываем ошибку, если не помещается
       free(tokens.head);
       return -1;
     }
@@ -312,7 +325,7 @@ int to_reverse_polish_notation(const char * expr, token_stack_t * polish_expr)
     buffer[i-first_digit] = '\0';
     tmp_token.kind = TT_NUMBER;
     tmp_token.number = atof(buffer);
-    assert(tmp_token.number != 0 || is_zero(buffer)); // if the algorithm is correct, this is always true
+    assert(tmp_token.number != 0 || is_zero(buffer)); // и все та же проверка на правильность алгоритма
     error_st = push(polish_expr, tmp_token);
     if (error_st < 0) {
       free(tokens.head);
@@ -349,10 +362,7 @@ int compute_polish(const token_stack_t * expr, double * result) {
   }
 
   token_stack_t tokens = {NULL, 0, 0};
-  token_t tmp_token; //this can just be numbers, since we never add operations there, but that'd require 2 new
-  // push and pop functions which i don't think i can handle. if only there would be a way to define a function that
-  // takes different kinds of arguments.. like, a template function, that behaves differently on different types?
-  // nonsense!
+  token_t tmp_token;
   tmp_token.kind = TT_NUMBER;
   int error_st;
   for (size_t i = 0; i < expr->real_size; ++ i) {
@@ -367,22 +377,21 @@ int compute_polish(const token_stack_t * expr, double * result) {
         break;
       case TT_FUNCTION:
         if (tokens.real_size == 0) {
-          free(tokens.head); // a function with no operand (e.g. 3 + (-))
+          free(tokens.head); // функция без операндов (e.g. 3 + (-))
           return -2;
         }
-        assert(tokens.head[tokens.real_size - 1].kind == TT_NUMBER); // we never add non-numbers
         tokens.head[tokens.real_size - 1].number =
           compute_function(expr->head[i].value, tokens.head[tokens.real_size - 1].number);
         break;
       case TT_OPERATION:
         if (tokens.real_size <= 1) {
-          free(tokens.head); // a binary operator with <=1 operand (e.g. 3+ or just +)
+          free(tokens.head); // бинарный оператор с <=1 операндом (напр., 3+ или +)
           return -2;
         }
-        // send them in the order we put them in stack! (stack[-2], stack[-1])
+        // отправляем в порядке добавления! (stack[-2], stack[-1])
         tmp_token.number = compute_operator(expr->head[i].value,
           tokens.head[tokens.real_size - 2].number, tokens.head[tokens.real_size - 1].number);
-        // pop once, the write the result into the second element (kinda like pop, pop, push)
+        // pop 1 раз, затем запись в последний элемент - то же, что и pop pop push
         error_st = pop(&tokens);
         if (error_st < 0) {
           free(tokens.head);
@@ -390,7 +399,7 @@ int compute_polish(const token_stack_t * expr, double * result) {
         }
         tokens.head[tokens.real_size - 1].number = tmp_token.number;
         break;
-      case TT_BRACE: // no braces!
+      case TT_BRACE: // в польской нотации нет скобок
         free(tokens.head);
         return -2;
         break;
@@ -411,8 +420,8 @@ int main() {
   char * line = NULL;
   token_stack_t polish_expr = {NULL, 0, 0};
   double res;
-  int error_st = read_expr(&line, false); // read until \n (true = until EOF)
-  if (error_st < 0) { // line is freed within read_expr
+  int error_st = read_expr(&line, false); // прочитать до \n (true = до EOF)
+  if (error_st < 0) { // если не удалось, line освобождается в read_expr
     printf("[error]");
     return 0;
   }
